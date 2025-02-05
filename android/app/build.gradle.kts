df131f3f471eed80dc5481c27ca77842ccec4f7d
Julia Samól <j.samol@papers.ch>
2025-02-05 16:20:58 +0000

    fix(jitpack): manually build FFI before build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -118,9 +118,3 @@ val ffiBuild: TaskProvider<Task> = tasks.register("ffiBuild", Task::class.java) 
         }
     }
 }
-
-tasks.configureEach {
-    if (name == "mergeDebugJniLibFolders" || name == "mergeReleaseJniLibFolders") {
-        dependsOn("ffiBuild")
-    }
-}
