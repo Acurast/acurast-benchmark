2cafaaff598705bae772c8b873cf1c27cbb1967d
Julia Samól <j.samol@papers.ch>
2025-05-06 17:24:18 +0200

    feat(ffi): allow for cpu simd configuration

--- rust/ffi/ffi.h
+++ rust/ffi/ffi.h
@@ -2,6 +2,7 @@
 #define ACUBENCH_FFI_H
 
 #include <cstdint>
+#include <stdbool.h>
 #include <stddef.h>
 
 #ifdef __cplusplus
@@ -32,6 +33,7 @@ extern "C" {
 
         size_t math_duration;
         size_t math_data_len;
+        bool math_simd;
 
         size_t sort_duration;
         size_t sort_data_len;
