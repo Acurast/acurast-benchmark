893cdf8dffa41bb5f6a2f3d215824bbfb1cebab9
Mike Godenzi <m.godenzi@papers.ch>
2025-05-08 15:22:13 +0200

    fix: specifying ios min version in build.rs

--- rust/build.rs
+++ rust/build.rs
@@ -1,7 +1,13 @@
 fn main() {
-    cc::Build::new()
-        .file("ffi/ffi.cpp")
-        .include("ffi")
-        .cpp(true)
-        .compile("ffi");
+    let target = std::env::var("TARGET").unwrap();
+    let mut build = cc::Build::new();
+
+    build.file("ffi/ffi.cpp").include("ffi").cpp(true);
+
+    if target.contains("apple-ios") {
+        build.flag("-mios-version-min=15.6");
+    }
+
+    build.compile("ffi");
 }
+
